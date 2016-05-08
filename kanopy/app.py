2afe140c066ced5f9d18cf85cebd3ac5ad5f3930
Kevin Yeh <kevinyeah@utexas.edu>
2016-05-08 01:58:48 -0500

    Add info scraping, cmd line stuff

--- kanopy/app.py
+++ kanopy/app.py
@@ -3,9 +3,11 @@ import certifi
 import json
 import requests
 import robobrowser
+import shutil
 from getpass import getpass
 
 cert = certifi.where()
+base_url = "https://utexas-kanopystreaming-com.ezproxy.lib.utexas.edu"
 login_url = "https://login.ezproxy.lib.utexas.edu/login?qurl=https%3a%2f%2futexas.kanopystreaming.com%2fcatalog"
 catalog_url = "https://utexas-kanopystreaming-com.ezproxy.lib.utexas.edu/catalog/?space=videos&page={}&rows=20&sort=most-popular"
 
@@ -26,22 +28,81 @@ def scrape_titles(browser, page):
     json_titles = [{'title': el.text, 'href': el.get('href')} for el in titles]
     return json_titles
 
-def main():
+@asyncio.coroutine
+def scrape_info(browser, title, href):
+    browser.open(href, verify=cert)
+    info = browser.select('#tab-desc')[0]
+    description = '\n\n'.join([p.text for p in info.find_all('p')])
+    clips = browser.select('#playlist-clips .clip-container.clip.asset')
+    features = info.find_all('li')
+    if not clips:
+        [runtime, year] = features[0].find('span').text.split(', ')
+        title['runtime'] = runtime
+        title['year'] = year
+    else:
+        clip_names = [clip.find(class_='clip-title').text for clip in clips]
+        runtimes = [clip.find(class_='clip-running-time').text for clip in clips]
+        yops = [clip.get('data-yop') for clip in clips]
+        title['clips'] = [{'title': clip_names[i],
+                           'runtime': runtimes[i],
+                           'year': yops[i]}
+                          for i in range(0, len(clips))]
+        
+    filmmakers = [a.text for a in features[1].find_all('a')]
+    languages = [a.text for a in features[-1].find_all('a')]
+    title['description'] = description
+    title['filmmakers'] = filmmakers
+    title['languages'] = languages
 
-    browser = login()
+    if len(features) > 3:
+        ft = [a.text for a in features[2].find_all('a')]
+        title['features'] = ft
+    
+    return title
 
+def update_titles(browser):
     futures = []
     for i in range(0, 765):
         future = asyncio.ensure_future(scrape_titles(browser, i))
-        futures.append(future)
+        futures.append(future)    
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(asyncio.wait(futures))
     loop.close()
 
     flattened = [item for future in futures for item in future.result()]
+    return flattened
+
+def update_info(browser):
+    with open('titles.txt', 'r') as outfile:
+        items = json.load(outfile)
+        futures = [asyncio.ensure_future(scrape_info(browser, item, base_url + item['href']))
+                   for item in items]
+        
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(asyncio.wait(futures))
+        loop.close()
+
+        extracted = [future.result() for future in futures]
+        return extracted
+        
+def main():
+    
+    if len(sys.argv) < 2:
+        print("Usage: python app.py (titles | info)")
+        sys.exit(0)
+
+    browser = login()
+    
+    titles = []
+    if sys.argv[1] == "titles":
+        titles = update_titles(browser)
+    else:
+        titles = update_info(browser)
+
+    shutil.copy('titles.txt', '.titles.txt')
     with open('titles.txt', 'w') as outfile:
-        json.dump(flattened, outfile)
+        json.dump(titles, outfile)
 
 if __name__ == '__main__':
     main()
